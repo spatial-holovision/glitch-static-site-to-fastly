0b2c57d9eaf7d9811da93857531dc675373b6b93
Sue Smith <sue@benormal.info>
2025-05-22 17:31:14 +0100

    Update serve.sh

--- _migrate/serve.sh
+++ _migrate/serve.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# This script runs when you click the Share button!
+# This script runs when you click the Serve button!
 
 npm run build 
 npx --yes @fastly/compute-js-static-publish@latest --root-dir=./build --kv-store-name=website-content 
 cd compute-js 
 npm run dev:publish 
-npm run dev:start || printf "\n\n 🤖 Hi! Local server running already, open PORTS and click the little Preview button on the Forwarded Address for Compute\n\n"
+npm run dev:start || printf "\n\n 🤖 Hi! You can only run the local Compute server once per session.\n\n"
