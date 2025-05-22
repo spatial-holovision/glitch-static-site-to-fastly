bdaddce9b07995d20017780ff0fed428a425f6e8
Sue Smith <sue@benormal.info>
2025-05-22 17:37:40 +0100

    Delete _migrate/serve.sh

--- _migrate/serve.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script runs when you click the Serve button!
-
-npm run build 
-npx --yes @fastly/compute-js-static-publish@latest --root-dir=./build --kv-store-name=website-content 
-cd compute-js 
-npm run dev:publish 
-npm run dev:start || printf "\n\n 🤖 Hi! You can only run the local Compute server once per session.\n\n"
